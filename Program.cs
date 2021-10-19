96f6bcb8872c0cd3a0522e4e8e6eeb427fad5880
Alain Trépanier <alain.trepanier@gmail.com>
2021-10-18 23:27:28 -0400

    Updated Emvie

--- Program.cs
+++ Program.cs
@@ -34,17 +34,18 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Data;
+using System.Data.Common;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 
-//using Emvie;
+using Emvie;
 using static Emvie.Convertion; // To get the DataTable ToCsv extension
 //using cnv = Emvie.Convertion;
 namespace Test {
 	public class ProductOwner {
-		[Range(5, 15)]
+		[Range(5,15)]
 		public int Id { get; set; }
 		[Required]
 		public string Name { get; set; }
@@ -55,17 +56,18 @@ namespace Test {
 		public static readonly IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
 		static async Task Main(string[] args_a)
 		{
+			InitDatabase();
+
 			// https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration/?view=aspnetcore-5.0#getvalue
 			var po = config.GetSection("ProductOwner").Get<ProductOwner>();
 			//var po = config.GetValue<ProductOwner>("ProductOwner");
-			if (!Validator.TryValidateObject(po, new ValidationContext(po), new List<ValidationResult>(), true))
-				throw new Exception("Unable to find all settings");
-			Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), config.GetValue<string>("ConsoleForegroundColor"), true);
+			if (!Validator.TryValidateObject(po,new ValidationContext(po),new List<ValidationResult>(),true)) throw new Exception("Unable to find all settings");
+			Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor),config.GetValue<string>("ConsoleForegroundColor"),true);
 			Console.WriteLine($"Config: Console color is {config.GetValue<string>("ConsoleForegroundColor")}\nProductOwner is {po}");
 
 			{
 				var msg = "Test Dictionary";
-				Console.WriteLine($"\n--- {msg} {new String('-', Math.Max(65 - msg.Length, 3))}\n");
+				Console.WriteLine($"\n--- {msg} {new String('-',Math.Max(65 - msg.Length,3))}\n");
 				var da = new Dictionary<string,int>(); // it has hashtable
 				da["Oranges"] = 67;
 				var db = new ConcurrentDictionary<string,int>(); // this one is thread safe and has more features
@@ -75,107 +77,110 @@ namespace Test {
 
 			{
 				var msg = "Test Read to \"string[]\" from appsettings.json";
-				Console.WriteLine($"\n--- {msg} {new String('-', Math.Max(65 - msg.Length, 3))}\n");
+				Console.WriteLine($"\n--- {msg} {new String('-',Math.Max(65 - msg.Length,3))}\n");
 				var animals = config.GetSection("Animals").Get<string[]>();
-				Console.WriteLine($"Animals: {string.Join(',', animals)}");
+				Console.WriteLine($"Animals: {string.Join(',',animals)}");
 			}
 
 			{
 				var msg = "Test Read to \"(string,string)[]\" from appsettings.json";
-				Console.WriteLine($"\n--- {msg} {new String('-', Math.Max(65 - msg.Length, 3))}\n");
+				Console.WriteLine($"\n--- {msg} {new String('-',Math.Max(65 - msg.Length,3))}\n");
 
 				(string Category, string Produce)[] items = config.GetSection("Items")
-				   .Get<Dictionary<string, string>[]>()
-			   .SelectMany(d => d) // Flatten from Dictionary[] to KeyValuePair[]
-			   .Select(kvp => (kvp.Key, kvp.Value))
+				   .Get<Dictionary<string,string>[]>()
+			   	.SelectMany(d => d) // Flatten from Dictionary[] to KeyValuePair[]
+				   .Select(kvp => (kvp.Key, kvp.Value))
 					.ToArray();
 				foreach (var item in items) Console.WriteLine($"({item.Category},{item.Produce})");
 			}
 
 			{
 				var msg = "Test Read to \"List<(string,string[])>\" from appsettings.json";
-				Console.WriteLine($"\n--- {msg} {new String('-', Math.Max(65 - msg.Length, 3))}\n");
+				Console.WriteLine($"\n--- {msg} {new String('-',Math.Max(65 - msg.Length,3))}\n");
 
 				List<(string Category, string[] Produces)> products = config.GetSection("Products")
-			   .Get<Dictionary<string, string[]>[]>()
-			   .SelectMany(i => i) // Flatten from Dictionary[] to KeyValuePair[]
+				   .Get<Dictionary<string,string[]>[]>()
+				   .SelectMany(i => i) // Flatten from Dictionary[] to KeyValuePair[]
 					.Select(i => (i.Key, i.Value)) // .Select(i => new ValueTuple<string, string[]>(i.Key, i.Value))
-			   .ToList();
+					.ToList();
+
 				foreach (var product in products) {
 					Array.Sort(product.Produces);
-					Console.WriteLine($"({product.Category},[{String.Join(',', product.Produces)}])");
+					Console.WriteLine($"({product.Category},[{String.Join(',',product.Produces)}])");
 				}
 			}
 
 			{
 				var msg = "Test Read to \"List<(string,string,string)>\" from appsettings.json";
-				Console.WriteLine($"\n--- {msg} {new String('-', Math.Max(65 - msg.Length, 3))}\n");
+				Console.WriteLine($"\n--- {msg} {new String('-',Math.Max(65 - msg.Length,3))}\n");
 
-				List<(string nme,string ado,string con)> dataSources = config.GetSection("DataSources")
-			   .Get<Dictionary<string, Dictionary<string, string>>[]>()
-				.SelectMany(i => i)
-				.Select(i => ValueTuple.Create(i.Key,i.Value.First().Key,i.Value.First().Value))
-				.ToList();
+				List<(string nme, string ado, string con)> dataSources = config.GetSection("DataSources")
+					.Get<Dictionary<string,Dictionary<string,string>>[]>()
+					.SelectMany(i => i)
+					.Select(i => ValueTuple.Create(i.Key,i.Value.First().Key,i.Value.First().Value))
+					.ToList();
+				Emvie.Db.Init(dataSources);
 				foreach (var ds in dataSources) Console.WriteLine($"{ds}");
+
 			}
 
 			// Dictionary to list of System.ValueTuple
 			{
 				var msg = "Test Dictionary to System.ValueTuple";
-				Console.WriteLine($"\n--- {msg} {new String('-', Math.Max(65 - msg.Length, 3))}\n");
-				List<(long a, int b)> tuples = (new Dictionary<long, int>() { { 1L, 1 }, { 2L, 2 } }).Select(x => (x.Key, x.Value)).ToList();
+				Console.WriteLine($"\n--- {msg} {new String('-',Math.Max(65 - msg.Length,3))}\n");
+				List<(long a, int b)> tuples = (new Dictionary<long,int>() { { 1L,1 },{ 2L,2 } }).Select(x => (x.Key, x.Value)).ToList();
 				foreach (var tuple in tuples) Console.WriteLine($"({tuple.a},{tuple.b})");
 			}
 
 			// Test System.ValueTuple
 			{
 				var msg = "Test System.ValueTuple";
-				Console.WriteLine($"\n--- {msg} {new String('-', Math.Max(65 - msg.Length, 3))}\n");
-				(string nme,string ado,string con)[] dataSources = {("One","SqlServer","Source=..."),("Two","MySql","Source=...")};
+				Console.WriteLine($"\n--- {msg} {new String('-',Math.Max(65 - msg.Length,3))}\n");
+				(string nme, string ado, string con)[] dataSources = { ("One", "SqlServer", "Source=..."),("Two", "MySql", "Source=...") };
 				foreach (var item in dataSources) Console.WriteLine($"{item}");
 			}
 
 			// Test System.ValueTuple
 			{
 				var msg = "Test linq's SelectMany";
-				Console.WriteLine($"\n--- {msg} {new String('-', Math.Max(65 - msg.Length, 3))}\n");
-				var names = new List<string>() { "Alain", "Trépanier" };
+				Console.WriteLine($"\n--- {msg} {new String('-',Math.Max(65 - msg.Length,3))}\n");
+				var names = new List<string>() { "Alain","Trépanier" };
 				var namesFlat = names.SelectMany(x => x);
 				foreach (char c in namesFlat) Console.Write(c + " ");
 				Console.Write("\n");
 			}
 
-			const int dices=1;
+			const int dices = 1;
 			var result = new List<int[]>();
 			result.Add(new int[dices]); // add first combination
 			Array.Fill(result[0],1);
-			for (;;) {
-				var lastResult = result[result.Count-1];
+			for (; ; ) {
+				var lastResult = result[result.Count - 1];
 				var newResult = new int[dices];
 				lastResult.CopyTo(newResult,0);
 				result.Add(newResult);
 
-				for (int i=0; i<dices ;i++) {
-					if (newResult[i] < 6) {newResult[i]++;break;}
-					if (i==dices-1 && newResult[dices-1]==6) goto A;
+				for (int i = 0; i < dices; i++) {
+					if (newResult[i] < 6) { newResult[i]++; break; }
+					if (i == dices - 1 && newResult[dices - 1] == 6) goto A;
 					newResult[i] = 1;
 				}
 			}
-			A:
-			var total = new int[2*dices+1];
+		A:
+			var total = new int[2 * dices + 1];
 			Array.Fill(total,0);
-			for(int i=0; i<Math.Pow(6,dices) ;i++) {
+			for (int i = 0; i < Math.Pow(6,dices); i++) {
 				int lineTotal = 0;
-				Console.Write($"\n{i+1,4}: ");
-				for (int j=0; j<dices ;j++) {
+				Console.Write($"\n{i + 1,4}: ");
+				for (int j = 0; j < dices; j++) {
 					Console.Write($"{result[i][j],-3}");
-					if (result[i][j] == 6) lineTotal+=2;
+					if (result[i][j] == 6) lineTotal += 2;
 					else if (result[i][j] > 3) lineTotal++;
 				}
 				total[lineTotal]++;
 			}
 			Console.Write("\n----\n");
-			for (int i=0; i<total.Length ;i++) Console.WriteLine($"{i,-4}: {total[i],6}{total[i]/Math.Pow(6,dices)*100,10:0.00}");
+			for (int i = 0; i < total.Length; i++) Console.WriteLine($"{i,-4}: {total[i],6}{total[i] / Math.Pow(6,dices) * 100,10:0.00}");
 
 			// *** The other tests ***
 			//TestAsync.Tester.Go(".");
@@ -191,16 +196,30 @@ namespace Test {
 			//TestXquery.Tester.Go();
 			//Console.WriteLine(DateTime.Now.Date); // How to havie it is OS format ?
 		}
+
+		static void InitDatabase()
+		{
+			DbProviderFactories.RegisterFactory("MySql.Data.MySqlClient", MySql.Data.MySqlClient.MySqlClientFactory.Instance);
+
+			//Db.Init(new Emvie.DataSource[] { new("MySqlSrvA", "MySql.Data.MySqlClient", "DataSource=localhost;port=3306;Database=Skillango;uid=root;pwd=1111qqqq;program_name=test") });
+
+			List<(string nme, string ado, string con)> dataSources = config.GetSection("DataSources")
+				.Get<Dictionary<string, Dictionary<string, string>>[]>()
+				.SelectMany(i => i)
+				.Select(i => ValueTuple.Create(i.Key, i.Value.First().Key, i.Value.First().Value))
+				.ToList();
+			Db.Init(dataSources);
+		}
 		static void TestCsv()
 		{
-			Console.WriteLine($"\n--- TestCsv {new String('-', 50)}\n");
+			Console.WriteLine($"\n--- TestCsv {new String('-',50)}\n");
 			DataTable tbl;
-			using (var sr = new StreamReader("Test.csv")) tbl = ConvertCsvToDataTable(sr, new string[] { "first", "last", "birth date" });
+			using (var sr = new StreamReader("Test.csv")) tbl = ConvertCsvToDataTable(sr,new string[] { "first","last","birth date" });
 			// var ms = new MemoryStream();
 			// using (FileStream fs = new FileStream("Test.csv", FileMode.Open, FileAccess.Read)) fs.CopyTo(ms);
 			// ms.Seek(0, SeekOrigin.Begin);
 			// using (var sr = new StreamReader(ms)) tbl = ConvertCsvToDataTable(sr, new string[] { "first", "last", "birth date" });
-			foreach (DataColumn c in tbl.Columns) Console.Write($"{c.ColumnName,-20}"); Console.Write($"\n{new String('-', 20 * tbl.Columns.Count)}\n");
+			foreach (DataColumn c in tbl.Columns) Console.Write($"{c.ColumnName,-20}"); Console.Write($"\n{new String('-',20 * tbl.Columns.Count)}\n");
 			foreach (DataRow r in tbl.Rows) {
 				foreach (DataColumn c in tbl.Columns) Console.Write($"{r[c.Ordinal],-20}");
 				Console.Write("\n");
@@ -209,7 +228,7 @@ namespace Test {
 			string csv = tbl.ToCsv();
 			Console.ForegroundColor = ConsoleColor.Magenta;
 			Console.Write($"\n---\n{csv}\n---\n");
-			Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), config.GetValue<string>("ConsoleForegroundColor"), true);
+			Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor),config.GetValue<string>("ConsoleForegroundColor"),true);
 		}
 	}
 }
--- TestDatabase.cs
+++ TestDatabase.cs
@@ -15,25 +15,25 @@ namespace TestDatabase {
 			Console.WriteLine($"\n--- {msg} {new String('-', Math.Max(65-msg.Length,3))}\n");
 
 			{  // Register the ADO.net adapter for MySql (using MySql namespace)
-            DbProviderFactories.RegisterFactory("MySql", MySql.Data.MySqlClient.MySqlClientFactory.Instance);
+            //DbProviderFactories.RegisterFactory("MySql.Data.MySqlClient", MySql.Data.MySqlClient.MySqlClientFactory.Instance);
 				// Register the ADO.net adapter for Microsoft Sql Server
 				// DbProviderFactories.RegisterFactory("SqlSrv", System.Data.SqlClient.SqlClientFactory.Instance);
 				// Register the ADO.net adapter for Oracle
 				// DbProviderFactories.RegisterFactory("Oracle", Oracle.ManageDataAccess.Client.MySqlClientFactory.Instance);
 
-         	Db.Init(new Emvie.DataSource[] {new ("MySql","MySql","DataSourcE=localhost;port=3306;Database=Skillango;uid=root;pwd=1111qqqq")});
-            var tbl = Db.ToTbl("MySql",0,"select Name, Manager, Title from Skillango.User order by Name limit 20;",EFillOption.Data,0,15);
+         	//Db.Init(new Emvie.DataSource[] {new ("MySql","MySql","DataSourcE=localhost;port=3306;Database=Skillango;uid=root;pwd=1111qqqq")});
+            var tbl = Db.ToTbl("MySqlA",0,"select Name, Manager, Title from Skillango.User order by Name limit 20;",EFillOption.Data,0,15);
             foreach (DataRow dr in tbl.Rows) Console.WriteLine($"{dr[0]}{new String(' ', Math.Max(40-((string)dr[0]).Length,3))}{dr[1]}{new String(' ', Math.Max(30-((string)dr[1]).Length,3))}{dr[2]}");
 
-            Db.BeginTransaction("MySql");
-            int i = (int)Db.ExecNonQry("MySql",0,"update Skillango.User set Title='Cyber Security Specialist' where Name='Alain Trépanier (6036527)';",0);
-            Db.CommitTransaction("MySql");
+            Db.BeginTransaction("MySqlA");
+            int i = (int)Db.ExecNonQry("MySqlA",0,"update Skillango.User set Title='Cyber Security Specialist' where Name='Alain Trépanier (6036527)';",0);
+            Db.CommitTransaction("MySqlA");
 
-            long count = (long)Db.ExecScalar("MySql",0,"select count(*) from Skillango.User where Name like @NameMask;",0,"@NameMask","a%");
+            long count = (long)Db.ExecScalar("MySqlA",0,"select count(*) from Skillango.User where Name like @NameMask;",0,"@NameMask","a%");
 
-            tbl = Db.ToTbl("MySql",0,"insert into Skillango.log (Type,Message) values ('Warning','Msg6'), ('Warning',@msg7); select ROW_COUNT(), LAST_INSERT_ID() as FirstAutoInc;",EFillOption.Data,0,0,"@msg7","Message 7");
+            tbl = Db.ToTbl("MySqlA",0,"insert into Skillango.log (Type,Message) values ('Warning','Msg6'), ('Warning',@msg7); select ROW_COUNT(), LAST_INSERT_ID() as FirstAutoInc;",EFillOption.Data,0,0,"@msg7","Message 7");
 
-            Console.WriteLine($"\n{i};{Db.ServerAndDbName("MySql")};count={count}; RowCount: {tbl.Rows[0][0]}; FirstAutoInc: {tbl.Rows[0]["FirstAutoInc"]}");
+            Console.WriteLine($"\n{i};{Db.ServerAndDbName("MySqlA")};count={count}; RowCount: {tbl.Rows[0][0]}; FirstAutoInc: {tbl.Rows[0]["FirstAutoInc"]}");
 			}
 		}
 	}
