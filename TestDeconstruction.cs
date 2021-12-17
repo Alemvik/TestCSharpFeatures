6777f57b2f6b8bba8cfb9b179216dc16d4d7a3c7
Alain Trépanier <alain.trepanier@gmail.com>
2021-12-17 16:25:44 -0500

    More on TestDeconstruction

--- TestDeconstruction.cs
+++ TestDeconstruction.cs
@@ -11,11 +11,24 @@ namespace TestDeconstruction {
 
 			var (name,age) = new Dictionary<string, ushort>{{"Alain Trépanier",54},{"Évie Dutel",13}}.First();
 
-			var (titre, autheur, isbn) = new Book("Barbara Hambly", "Dragonsbane","12345");
+			var (titre, autheur, isbn) = new Book("Barbara Hambly", "Dragonsbane","0-345-31572-3");
+			var (t, _, _) = new Book("Conan the Bold", "John Maddox Roberts","0-8125-5210-5");
+			var (n, b) = new Dog {Name="Togo", BirthDay = new DateOnly(1913,10,17) };
 
-			Console.WriteLine($"name/age = {name}/{age}; titre/autheur/isbn = {titre}/{autheur}/{isbn}");
+
+			Console.WriteLine($"name/age = {name}/{age}\ntitre/autheur/isbn = {titre}/{autheur}/{isbn}\nt = {t}\nn/b = {n}/{b:yyyy-MM-dd}");
 		}
 	}
 
-	public record Book (string Author, string Title, string Isbn);
+	record Book (string Author, string Title, string Isbn);
+
+	class Dog {
+		public string Name {get; init;} = default!; // The null-forgiving operator has no effect at run time. It only affects the compiler's static flow analysis by changing the null state of the expression. At run time, expression x! evaluates to the result of the underlying expression x.
+		public DateOnly BirthDay {get; init;}
+
+		public void Deconstruct(out string name, out DateOnly birthDay) {
+			name = Name;
+			birthDay = BirthDay;
+		}
+	}
 }
